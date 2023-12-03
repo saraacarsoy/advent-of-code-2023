f93ac4d1bb8db31fc2003cac209df42a8370c08c
saraacarsoy <43539414+saraacarsoy@users.noreply.github.com>
2023-12-03 19:05:09 +0100

    Added day 3 part 2
    im never doing this again

--- day3/day3.js
+++ day3/day3.js
@@ -23,6 +23,7 @@ function isNonAlphanumeric(value) {
 
 function checkNumber(arr) {
     let sum = 0;
+    let starArr = [];
 
     for (let i = 0; i < arr.length; i++) {
         let numbers = "";
@@ -33,7 +34,8 @@ function checkNumber(arr) {
 
             if (isNumeric(currentValue)) {
                 numbers += currentValue;
-                conditionArr.push(checkNeighbors(arr, i, j));
+                //conditionArr.push(checkNeighbors(arr, i, j));
+                starArr.push(checkNeighbors(arr, i, j));
             } else {
                 if (hasTrueValue(conditionArr)) {
                     sum += +numbers;
@@ -49,7 +51,10 @@ function checkNumber(arr) {
         }
     }
 
-    console.log(sum)
+    const entries = formatStarArray(starArr);
+    const multiplications = calculateGearRatio(entries);
+
+    console.log(sumMultiplications(multiplications))
 }
 
 function checkNeighbors(arr, x, y) {
@@ -65,18 +70,98 @@ function checkNeighbors(arr, x, y) {
     ];
 
     let validNeighbors = 0;
+    let starCoordinates = [];
+    let totalNumber = "";
 
     for (const [a, b] of coordinates) {
         if (a > 0 && a < arr.length && b > 0 && b < arr[0].length) {
-            if (isNonAlphanumeric(arr[a][b])) {
-                validNeighbors++;
+            // if (isNonAlphanumeric(arr[a][b])) {
+            //     validNeighbors++;
+            // }
+
+            if(arr[a][b] === "*") {
+                const leftNeighbor = arr[x][y-1]
+                const rightNeighbor = arr[x][y+1]
+                const furtherLeftNeighbor = arr[x][y-2]
+                const furtherRightNeighbor = arr[x][y+2]
+
+                if(leftNeighbor != undefined && isNumeric(leftNeighbor) && furtherLeftNeighbor != undefined && isNumeric(furtherLeftNeighbor)) {
+                    totalNumber += furtherLeftNeighbor;
+                    totalNumber += leftNeighbor;
+                }
+
+                if(!isNumeric(furtherLeftNeighbor) && leftNeighbor != undefined && isNumeric(leftNeighbor)) {
+                    totalNumber += leftNeighbor;
+                }
+
+                totalNumber += arr[x][y];
+
+                if(rightNeighbor != undefined && isNumeric(rightNeighbor)) {
+                    totalNumber += rightNeighbor;
+
+                    if(furtherRightNeighbor != undefined && isNumeric(furtherRightNeighbor)) {
+                        totalNumber += furtherRightNeighbor;
+                    }
+                }
+                
+                starCoordinates.push({star: [a, b], number: parseInt(totalNumber)});
+                
             }
         }
     }
 
-    return validNeighbors > 0;
+    // return validNeighbors > 0;
+    return starCoordinates;
 }
 
 function hasTrueValue(arr) {
     return arr.some(value => value === true);
+}
+
+function formatStarArray(starArr) {
+    coordinates = starArr.filter(arr => arr.length > 0).reduce((acc, curr) => {
+        const exists = acc.some(obj =>
+            obj.star[0] === curr[0].star[0] &&
+            obj.star[1] === curr[0].star[1] &&
+            obj.number === curr[0].number
+        );
+
+        if (!exists) {
+        acc.push(curr[0]);
+        }
+        return acc;
+    }, []);
+
+    return coordinates;
+} 
+
+function calculateGearRatio(arr) {
+    const groupedStars = {};
+
+    arr.forEach(obj => {
+        const key = obj.star.toString();
+        if (!groupedStars[key]) {
+          groupedStars[key] = [];
+        }
+        groupedStars[key].push(obj.number);
+    });
+
+    const result = [];
+    for (const key in groupedStars) {
+        if (groupedStars[key].length > 1) {
+            const numbers = groupedStars[key];
+            const multiplied = numbers.reduce((acc, num) => acc * num, 1);
+            result.push({ star: key.split(",").map(Number), multiplied });
+        }
+    }
+
+    return result;
+}
+
+function sumMultiplications(arr) {
+    total = 0;
+
+    arr.forEach((val) => total += val.multiplied);
+
+    return total;
 }
