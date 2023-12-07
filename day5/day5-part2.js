0470cf7b215f0a0c57d75a41f51e0f0753f9242e
SAcarsoy <saraacarsoy@sabanciuniv.edu>
2023-12-07 14:13:55 +0100

    Finally working part 2! Also updated the part 1

--- day5/day5-part2.js
+++ day5/day5-part2.js
@@ -3,22 +3,23 @@ const { config } = require('process');
 const f = fs.readFileSync('day5.txt', 'utf-8');
 
 function getSeedId() {
-    const input = f.split(/\r\n\r\n/);
+    const input = f.split(/\r?\n\n/);
+    // const input = f.split(/\r\n\r\n/);
     const seedRow = input[0];
     const seedsArr = seedRow.match(/seeds: (\d+(?: \d+)*)/);
     const seedValues = seedsArr[1].split(' ').map(Number);
-
     const humToLocArr = getLowestOrderLocsFromInput(input[7]);
 
     for(let i=0; i<humToLocArr.length; i++) {
         for(let j=humToLocArr[i][0]; j<humToLocArr[i][0] + humToLocArr[i][2]; j++) {
+            console.log("checking location: ", j);
             let location = j;
             for (let k=input.length-1; k>0; k--) {
                 location = reverseCheckCurrentMap(location, input[k]);
             }
 
-            if (seedValues.includes(location)) {
-                return(location);
+            if (checkIfSeedInRange(seedValues, location)) {
+                return j;
             }
         }
     }
@@ -28,6 +29,7 @@ function reverseCheckCurrentMap(location, map) {
     let lines = map.split('\n');
     lines.shift();
     let mapConfigs = lines.map(line => line.split(' ').map(Number));
+    //let mapConfigs = map.split('\n').slice(1); 
     let newLocation = location;
 
     for (let i=0; i<mapConfigs.length; i++) {
@@ -35,7 +37,6 @@ function reverseCheckCurrentMap(location, map) {
             newLocation = location - mapConfigs[i][0] + mapConfigs[i][1];
         }
     }
-
 
     return newLocation;
 }
@@ -50,6 +51,15 @@ function getLowestOrderLocsFromInput(locationMap) {
     return result;
 }
 
-console.log("seed is: ", getSeedId());
+function checkIfSeedInRange(seedArr, seed) {
+    for (let i = 0; i <seedArr.length; i += 2) {
+        if (seed >= seedArr[i] && seed < seedArr[i] + seedArr[i+1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log("location is: ", getSeedId());
 
 // seeds: 79 14 55 13
