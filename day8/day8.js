01022268d96c15107b8cd5cb1c98f519f4075679
saraacarsoy <43539414+saraacarsoy@users.noreply.github.com>
2023-12-08 23:10:22 +0100

    part 2 brute force

--- day8/day8.js
+++ day8/day8.js
@@ -11,13 +11,29 @@ function readInput() {
         return [left, ...values];
     });
 
-    map.sort(sortNode);
-
-    checkNodes(map, directions)
+    // map.sort(sortNode);
+    map.sort(sortNodeFromEnd);
+    // checkNodes(map, directions);
+    checkEntryNodes(map, directions);
 }
 
-function sortNode(a, b){
+function sortNode(a, b) { //part 1
     return a[0].localeCompare(b[0]);
+}
+
+function sortNodeFromEnd(a, b) {
+    const lastLetterA = a[0].charAt(2);
+    const lastLetterB = b[0].charAt(2);
+  
+    if (lastLetterA < lastLetterB) {
+      return -1;
+    } 
+    else if (lastLetterA > lastLetterB) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
 }
 
 function checkNodes(map, directionArr) {
@@ -38,12 +54,33 @@ function checkNodes(map, directionArr) {
 
 }
 
+function checkEntryNodes(map, directionArr) {
+    const startNodes = [0, 1];
+    let targetNodes = startNodes.slice();
+    let count = 0;
+
+    while (!targetNodes.every(node => node >= map.length - 2)) {
+        for (let i = 0; i < directionArr.length; i++) {
+            for (let j = 0; j < startNodes.length; j++) {
+                const newNode = getTarget(map, targetNodes[j], directionArr[i]);
+
+                targetNodes[j] = newNode;
+            }
+            console.log("counting: ", count);
+            count += 1;
+        }
+        
+    }
+    
+    return count;
+}
+
+
 function getTarget(map, i, dir) {
     let target = dir === "L" ? map[i][1] : map[i][2];
     let targetIndex = map.findIndex(subArray => subArray[0] === target);
 
     return targetIndex;
 }
-
 
 readInput();
